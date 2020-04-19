58b58aeae77361dcbc877f015d7e8989fbfc4978
iffishells <iffishells@gmail.com>
2020-04-19 22:36:44 +0500

    Adding day06 file contain javascript code

---
+++ 6-Day/javascript/myjava.js
@@ -0,0 +1,19 @@
+//for checking its working or not!!!!
+console.log("Hellow World");
+var fname = prompt("Enter First Name :");
+var lname = prompt("Enter the last Name");
+var age = prompt("what is your age :");
+var email= prompt("Enter your Email :");
+console.log("Fname has Frist character : ",fname[0]);
+console.log("Lname has frist character : ",lname[0] );
+console.log("Customer has age : ",age);
+console.log("Customer has email is :",email);
+
+if(fname[0]==lname[0])
+{
+    console.log("Congralation you got the access of this websites")
+}
+else
+{
+    console.log("Get out from here")
+}
